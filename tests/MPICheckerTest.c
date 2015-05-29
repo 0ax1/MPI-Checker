40618429b4e314cd87c73164470b041ccb44e598
0ax1 <alexander.ra.droste@googlemail.com>
2015-05-29 16:46:15 +0200

    added tests for path sensitive checks

--- tests/MPICheckerTest.c
+++ tests/MPICheckerTest.c
@@ -25,7 +25,6 @@ void missingWait() {
     double buf = 0;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     if (rank == 1) {
-    } else if (rank == 2) {
     } else {
         MPI_Request sendReq1;
         MPI_Request recvReq1;
@@ -33,5 +32,30 @@ void missingWait() {
         MPI_Isend(&buf, 1, MPI_DOUBLE, rank + 1, 0, MPI_COMM_WORLD, &sendReq1); // expected-warning{{Nonblocking call using request sendReq1 has no matching wait. }}
         MPI_Irecv(&buf, 1, MPI_DOUBLE, rank - 1, 0, MPI_COMM_WORLD, &recvReq1);
         MPI_Wait(&recvReq1, MPI_STATUS_IGNORE);
+    }
+}
+
+void doubleNonblocking() {
+    int rank = 0;
+    double buf = 0;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    if (rank == 1) {
+    } else {
+        MPI_Request sendReq1;
+        MPI_Request recvReq1;
+
+        MPI_Isend(&buf, 1, MPI_DOUBLE, rank + 1, 0, MPI_COMM_WORLD, &sendReq1);
+        MPI_Irecv(&buf, 1, MPI_DOUBLE, rank - 1, 0, MPI_COMM_WORLD, &sendReq1); // expected-warning{{Request sendReq1 is already in use by nonblocking call MPI_Isend in line 47. }}
+        MPI_Wait(&sendReq1, MPI_STATUS_IGNORE);
+    }
+}
+
+void missingNonBlocking() {
+    int rank = 0;
+    double buf = 0;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    if (rank == 1) {
+        MPI_Request sendReq1;
+        MPI_Wait(&sendReq1, MPI_STATUS_IGNORE); // expected-warning{{Request sendReq1 has no matching nonblocking call.}}
     }
 }
