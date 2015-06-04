1cb20ad15e7da7ee753657eaa224bcf778efc179
0ax1 <alexander.ra.droste@googlemail.com>
2015-06-04 10:20:29 +0200

    array-visitor not testable with unit test

--- tests/unit_tests/ArrayVisitorTest.cpp
+++
@@ -1,8 +0,0 @@
-#include "gtest/gtest.h"
-#include "clang/AST/Decl.h"
-#include "ArrayVisitor.hpp"
-
-// TEST(ArrayVisitor, collectVars) {
-    // auto v = clang::VarDecl::Create;
-    // clang::VarDecl:
-// }
