2f5697ed7ea116661347dc8842bdc10c67d5bf83
0ax1 <alexander.ra.droste@googlemail.com>
2015-06-04 10:10:59 +0200

    added unit tests

---
+++ tests/unit_tests/ArrayVisitorTest.cpp
@@ -0,0 +1,8 @@
+#include "gtest/gtest.h"
+#include "clang/AST/Decl.h"
+#include "ArrayVisitor.hpp"
+
+// TEST(ArrayVisitor, collectVars) {
+    // auto v = clang::VarDecl::Create;
+    // clang::VarDecl:
+// }
