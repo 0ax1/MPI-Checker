902127ad7872073ca1e8448af58e67726f397314
Alexander Droste <alexander.ra.droste@googlemail.com>
2015-08-18 14:26:23 +0200

    update make example

--- examples/make/basic/basic.c
+++ examples/make/basic/basic.c
@@ -32,39 +32,49 @@
 
 double fnn() { return 22.22; }
 int f2n() { return 22; }
+int f() { return rand(); }
 
-int rank = 0;
+int rank;
 int buf;
 int N = 0;
 
-int f() { return rand(); }
+typedef struct {
+    int rank;
+    int other;
+} structWithRank;
 
 void communicate1() {
-    MPI_Request req1, req2;
+    structWithRank swr = {.rank = 0, .other = 0};
+    MPI_Comm_rank(MPI_COMM_WORLD, &swr.rank);
+    MPI_Request req[2];
 
+    if (swr.rank > 0) {
+        MPI_Isend(&buf, 1, MPI_INT, rank + 1, 1, MPI_COMM_WORLD, &req[0]);
+        MPI_Irecv(&buf, 1, MPI_INT, rank - 1, 1, MPI_COMM_WORLD, &req[1]);
+
+    }
+
+    if (swr.rank > 0) {
+        MPI_Wait(&req[0], MPI_STATUS_IGNORE);
+        MPI_Waitall(2, req, MPI_STATUS_IGNORE);
+    }
+}
+
+void communicate2() {
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    if (rank == 0) {
-        MPI_Isend(&buf, 1, MPI_INT, rank + 1, 0, MPI_COMM_WORLD, &req1);
-        MPI_Isend(&buf, 1, MPI_DOUBLE, rank + 1, 1, MPI_COMM_WORLD, &req2);
-
-        MPI_Request r[2] = {req1, req2};
-        MPI_Waitall(2, r, MPI_STATUSES_IGNORE);
-
-    } else if (rank == 1) {
-        MPI_Irecv(&buf, 1, MPI_INT, rank - 1, 0, MPI_COMM_WORLD, &req1);
-        MPI_Irecv(&buf, 1, MPI_INT, rank - 1, 0, MPI_COMM_WORLD, &req2);
-
-        MPI_Request r[2] = {req1, req2};
-        MPI_Waitall(2, r, MPI_STATUSES_IGNORE);
-
-        MPI_Wait(&req1, MPI_STATUS_IGNORE);
+    if (rank > 0) {
+        MPI_Send(&buf, 1, MPI_DOUBLE, rank + 1, 1, MPI_COMM_WORLD);
+    } else {
+        MPI_Recv(&buf, 1, MPI_INT, rank - 1, 2, MPI_COMM_WORLD,
+                 MPI_STATUS_IGNORE);
     }
 }
 
 int main(int argc, char *argv[]) {
     MPI_Init(&argc, &argv);
     communicate1();
+    communicate2();
 
     MPI_Finalize();
     return 0;
