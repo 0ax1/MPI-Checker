93ee57e20925899dc35d751c1c849474262205b1
Alexander Droste <alexander.ra.droste@googlemail.com>
2015-08-29 16:10:42 +0200

    removed eval toy example

--- eval/partdiff1/common-par.h
+++
@@ -1,6 +0,0 @@
-#ifndef COMMON_PAR_H_YQZFWM2U
-#define COMMON_PAR_H_YQZFWM2U
-
-void MPI_doSetup(int* argc, char*** argv, int* size, int* rank);
-
-#endif  // end of include guard: COMMON_PAR_H_YQZFWM2U
